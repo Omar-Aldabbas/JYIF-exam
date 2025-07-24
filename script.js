30ebef13935805ef2d72bfa2bd0dec630a4bab5d
Omar Aldabbas <omaraldabbassjo@gmail.com>
2025-07-24 10:51:26 -0700

    Clean

--- script.js
+++ script.js
@@ -2,31 +2,29 @@ const list = document.querySelector("#navbar__list");
 const sections = document.querySelectorAll("section");
 const nav = document.querySelector(".navbar__menu");
 
-console.log(list);
-// sections.forEach(el =>console.log(el))
-sections.forEach((el) => {
-  const id = el.id;
-  const label = el.dataset.nav;
+const getSections = () => {
+  sections.forEach((el) => {
+    const id = el.id;
+    const label = el.dataset.nav;
 
-  const newNav = `<li class="nav__item"><a  href="#${id}">${label}</a></li>`;
-  list.insertAdjacentHTML("beforeend", newNav);
-});
+    const newNav = `<li class="nav__item"><a  href="#${id}">${label}</a></li>`;
+    list.insertAdjacentHTML("beforeend", newNav);
+  });
+};
 
-////////////////////////////////////////////
-nav.addEventListener("click", (e) => {
-  e.preventDefault();
-  const link = e.target.closest("a[href^='#']");
+const smoothScrolling = () => {
+  nav.addEventListener("click", (e) => {
+    e.preventDefault();
+    const link = e.target.closest("a[href^='#']");
+    if (!link) return;
 
-  // console.log(link);
+    const idSec = link.getAttribute("href");
+    const section = document.querySelector(idSec);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+};
 
-  if (!link) return;
-
-  const idSec = link.getAttribute("href");
-  const section = document.querySelector(idSec);
-
-  // console.log(idSec, section);
-
-  if (section) {
-    section.scrollIntoView({ behavior: "smooth" });
-  }
-});
+getSections();
+smoothScrolling();
